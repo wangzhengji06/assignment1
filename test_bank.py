319ed83c76b87e2826ff4b249369aa5b0f86ef65
wangzhengji06 <wangzhengji06@gmail.com>
2025-11-08 21:32:10 +0900

    Fixed the bank test

--- test_bank.py
+++ test_bank.py
@@ -11,7 +11,7 @@ def test_backaccount_deposit():
     """
     Creates a back account and deposits 200 dollars in it.
     """
-    account1 = domain.BankAccount()
+    account1 = domain.BankAccount(0, 0, 0)
     account1.deposit(200)
     assert account1.get_balance() == 200
 
@@ -20,7 +20,7 @@ def test_backaccount_withdraw():
     """
     Creates a bank account and withdraw 100 dollars from it.
     """
-    account1 = domain.BankAccount()
+    account1 = domain.BankAccount(0, 0, 0)
     account1.deposit(300)
     account1.withdraw(200)
     assert account1.get_balance() == 100
@@ -30,7 +30,7 @@ def test_backaccount_withdraw_toomuch():
     """
     Creates a back account withdraing too much that trigger the error.
     """
-    account1 = domain.BankAccount()
+    account1 = domain.BankAccount(0, 0, 0)
     account1.deposit(200)
     account1.withdraw(300)
     assert account1.get_balance() == 200
